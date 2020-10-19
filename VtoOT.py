e1baf8326e30f8c135352dbca24c49799b552009
BudMacaulay <bm5g15@soton.ac.uk>
2020-10-19 10:56:50 +0100

    Pushing a new mini Hubbard U block. Should allow the easy access of U

--- VtoOT.py
+++ VtoOT.py
@@ -4,11 +4,18 @@ import sys
 from ase.io import read, write
 import os, argparse, logging
 import re
-from ase import atoms
 from mendeleev import element
+from dir2cart import dir2cart
+import json
 
 
-def VtoOT(input_pos, outputdir):
+# TODO - setup the arg parser - im way too lazy
+# TODO - Do the math to convert poscars that are currently in Direct format immediately into Cartesian format
+#  so you dont need a script before hand
+# TODO - consider a better way to check for T/S's (perhaps some regex is needed.)
+# TODO - Add a ngwf_num / ngwf_rad Auto filler just for convienience
+
+def VtoOT(input_pos, jsonfile, outputdir):
     """Converts a vasp POSCAR (including Ts/F's to a onetep xyz + a onetep dynblock (located in a file called [
     atomgeo]))  dyna ==True is not yet used and also it currently doesnt use.
 
@@ -17,24 +24,28 @@ def VtoOT(input_pos, outputdir):
     in which it'll force all atoms to be so
     """
 
-    #-------------------------------------------------------------------------------
+    # -------------------------------------------------------------------------------
     # Argument parser
-    #-------------------------------------------------------------------------------
-    #parser = argparse.ArgumentParser(description=vtoOT.__doc__,
+    # -------------------------------------------------------------------------------
+    # parser = argparse.ArgumentParser(description=VtoOT.__doc__,
     #                                 epilog="BM151020")
     # Positional arguments
-    #parser.add_argument('inputpos',
+    # parser.add_argument('inputpos',
     #                    default="POSCAR",
     #                    type=str, nargs='?',
     #                    help='set input POSCAR. Default is the one in this directory;')
     # Optional args
-    #parser.add_argument('--output',
+    # parser.add_argument('--output',
     #                    type=str,
     #                    action="store_true", dest=outputdir,
     #                    help='set an outputdirectory. Default is here too probably')
 
-    #args = parser.parse_args(argv)
-    listofchanged = []
+    # args = parser.parse_args(argv)
+
+    # Load the json file and read what is desired to be put in the .dat file.
+    with open('VtoOTconf.json', 'r') as f:
+        config2_dict = json.load(f)
+
     if input_pos.endswith("POSCAR"):
         print("READING POS")
     else:
@@ -49,25 +60,27 @@ def VtoOT(input_pos, outputdir):
 
     totnumb = readlines[6].split()
     totnumb = [int(x) for x in totnumb]
-    totnumb = sum(totnumb) # Total number of atoms.
+    totnumb = sum(totnumb)  # Total number of atoms.
     print(str(totnumb) + " ATOMS FOUND in POS")
-    atomslist = readlines[5].split() # Small list to iterate over and set right flags
-    write(outputdir + "/ATOMsites.xyz",read(input_pos)) # Saves an XYZ quickly since its good to do so.
+    atomslist = readlines[5].split()  # Small list to iterate over and set right flags
+    write(outputdir + "/ATOMsites.xyz", read(input_pos))  # Saves an XYZ quickly since its good to do so.
 
     # INITIAL CHECK - WILL REWORK TO DO MATH HERE AND AUTOCONV TO CART.
     if readlines[8].startswith("C") or readlines[8].startswith("c"):
         print("COOL")
     else:
-        print(" ENSURE LINE 8 is 'CARTESIAN' and atom co-ords are too!")
-        #exit()
-
+        print("Direct POSCAR found")
+        val = input("Shall i update your poscar to be cartesian? : Y/N")
+        if val.lower == "y":
+            dir2cart(input_pos)
+        else:
+            exit()
 
     # FINAL CHECK HERE! - If there wasn't an "Selective dynamics line it'll do its best"
     if readlines[7].startswith("S") or readlines[7].startswith("s"):
         print("dynamic vasp system detected creating correct BLOCK SPECIES.")
     else:
         print("no dyn line found - doing the best i can - expect bigprintouts.")
-
 
     latticeblock = ['%BLOCK LATTICE_CART\n', 'ang\n']
     for i in range(0, 3):
@@ -77,9 +90,8 @@ def VtoOT(input_pos, outputdir):
         latticeblock.append('  ' + stringer + '\n')
     latticeblock.append('%ENDBLOCK LATTICE_CART\n')
 
-
     posblock = []
-    for i in readlines[-totnumb:]: # This loop checks if each line ends in "T or F" pretty much
+    for i in readlines[-totnumb:]:  # This loop checks if each line ends in "T or F" pretty much
         if "T" in i[-10:]:
             posblock.append("_T {} \n".format(" ".join(i.split()[0:3])))
         elif "F" in i[-10:]:
@@ -87,7 +99,7 @@ def VtoOT(input_pos, outputdir):
         else:
             print("Line {} is not parsable, is there something other than F F F/T T T? at the end of the file")
             print("Saving as T anyway")
-            posblock.append("_F {} \n".format(i[:-7]))
+            posblock.append("_F {} \n".format(" ".join(i.split()[0:3])))
 
     posblock2 = []
     for i in range(0, len(atomslist)):
@@ -96,20 +108,25 @@ def VtoOT(input_pos, outputdir):
 
     posblock2 = ['%BLOCK POSITIONS_ABS\n', 'ang\n', *posblock2, '%ENDBLOCK POSITIONS_ABS\n']
 
-
     # Two important blocks remain - The constraints and the species they are both similar so piled together
     allspecies = []
     for i in posblock2[2:-1]:
-        allspecies.append(i[0:4].strip()) # Pull the first 4 letters(this is the upper limit for onetep labels anyway)
-    allspecies = list(set(allspecies)) # The list of the set of all atoms observed in POSCAR
+        allspecies.append(i[0:4].strip())  # Pull the first 4 letters(this is the upper limit for onetep labels anyway)
+    allspecies = list(set(allspecies))  # The list of the set of all atoms observed in POSCAR
     speciesblock = ["%BLOCK SPECIES\n"]
     constblock = ["%BLOCK SPECIES_CONSTRAINTS\n"]
     for spec in allspecies:
         if "_T" in spec:
             constblock.append("{} NONE\n".format(spec))
         elif "_F" in spec:
-            constblock.append("{} FIXED\n".format(spec))
-        speciesblock.append("{0} {1} {2} ngwf_num ngwf_rad\n".format(spec, spec[:-2], element(spec[:-2]).atomic_number)) # I have a mini json file that will strip atom numbers e.t.c.
+            constblock.append("{} FIXED\n".format(spec))  #
+
+        if config2_dict.get("Guess_NGWF") == True:  # As of current this does nothing. Will add a mini periodictable.
+            speciesblock.append("{0} {1} {2} ngwf_num ngwf_rad\n".format(spec, spec[:-2],
+                                                                         element(spec[:-2]).atomic_number))
+        else:
+            speciesblock.append("{0} {1} {2} ngwf_num ngwf_rad\n".format(spec, spec[:-2],
+                                                                         element(spec[:-2]).atomic_number))
 
     speciesblock.append("%ENDBLOCK SPECIES\n")
     constblock.append("%ENDBLOCK SPECIES_CONSTRAINTS\n")
@@ -117,16 +134,48 @@ def VtoOT(input_pos, outputdir):
     completeblock = [latticeblock, "\n", speciesblock, "\n", constblock, "\n", posblock2]
     completeblock = [val for sublist in completeblock for val in sublist]
 
+    # Checking JSON FLAGS FOR ADDITIONAL THINGS.
+    if config2_dict.get("HubbardU") is True:
+        print("HubbardU flag switched on saving a U block in the .dat")
+        Ublock = ["%BLOCK HUBBARD\n"]
+        if config2_dict.get("GuessHubbardU") is True:
+            for element in allspecies:
+                Ublock.append("{0} l^2 U(eV) J(eV) Z(-10) a(addforce) sig(spin)\n".format(element))
+        else:
+            for element in allspecies:
+                Ublock.append("{0} l^2 U(eV) J(eV) Z(-10) a(addforce) sig(spin)\n".format(element))
 
+        Ublock.append("%ENDBLOCK HUBBARD\n")
+    else:
+        Ublock = []  # too ensure no dead block is added
+
+    completeblock = [latticeblock, "\n", speciesblock, "\n", constblock, "\n", Ublock, "\n", posblock2]
+    completeblock = [val for sublist in completeblock for val in sublist]
+
+    if config2_dict.get("dynamic") is False:
+        print("Dynamic flag not switched on - making another simple dat file. ")
+        posblock2_simp = [x.replace("T", "F") for x in posblock2]
+        speciesblock_simp = [x for x in speciesblock if not re.search("_T", x)]  # Remove the T lines
+        constblock_simp = [x for x in constblock if not re.search("_T", x)]  # Remove the T lines -
+        Ublock_simp = [x for x in Ublock if not re.search("_T", x)]
+        completeblock_simp = [latticeblock, "\n", speciesblock_simp, "\n", constblock_simp, "\n", Ublock_simp, "\n", posblock2_simp]
+        completeblock_simp = [val for sublist in completeblock_simp for val in sublist]
+
+        with open(outputdir + "/ATOMsites_noDYN.dat", "w+") as out:
+            for line in completeblock_simp:
+                out.write(line)
 
     with open(outputdir + "/ATOMsites.dat", "w+") as outfile:
         for line in completeblock:
             outfile.write(line)
 
 
-if __name__ == "__main__": #Stuff below here is here until I can come up with an adequate parser
-    if len(sys.argv) == 2:
-        VtoOT(input_pos=sys.argv[1], outputdir="/".join(sys.argv[1].split("/")[:-1]))
+if __name__ == "__main__":  # Stuff below here is here until I can come up with an adequate parser
+    if len(sys.argv) == 3:
+        VtoOT(input_pos=sys.argv[1], jsonfile=sys.argv[2], outputdir="/".join(sys.argv[1].split("/")[:-1]))
 
-    elif len(sys.argv) == 3:
-        VtoOT(input_pos=sys.argv[1], outputdir=sys.argv[2])
+    elif len(sys.argv) == 4:
+        VtoOT(input_pos=sys.argv[1], jsonfile=sys.argv[2], outputdir=sys.argv[3])
+
+    else:
+        print("Unknown number of sys variables. - Read the tutorial.")
